35139a0204f085d4d5ee64b91ff8faade6ead338
hoangdesu <hoangdesu@gmail.com>
2024-09-15 14:01:47 +0700

    while loop, homework

---
+++ sep15-while.py
@@ -0,0 +1,53 @@
+# allow the user to keep entering a number
+# multiply them all up
+# until user enters 0, stop the loop and display the output
+# following the template: 
+# 'Total = 4 x 6 x 2 = 48'
+
+# fav_num = 7
+
+# guess_num = int(input('Guess my favorite num: '))
+
+# while guess_num != fav_num:
+#     guess_num = int(input('Wrong. Guess my favorite num again: '))
+
+# print('Correct. My favorite number is ' + str(fav_num))
+
+# num = int(input('Enter a number: '))
+# total = 1
+# output = 'Total = '
+
+# while num != 0:
+#     # print('inside loop:', num)
+    
+#     total = total * num
+    
+#     output = output + str(num)
+    
+#     # after all, update num from user input
+#     num = int(input('Enter a number: '))
+    
+#     if num != 0:
+#         output += ' x '
+
+    
+# # print('outside while loop')
+# output += f' = {total}'
+
+# print(output)
+# print(total)
+
+
+# Login app
+
+# allow user to enter a username and a password
+
+# print('hello')
+
+# allow to user attempt 5 times
+
+Login failed. You have 3 more attempts
+
+Login failed. You have 1 more attempt
+
+Login failed. Your account has been locked -> quit the loop
