c36aa163ab93fe2a6ac9f28e856918d44b2f96cf
hoangdesu <hoangdesu@gmail.com>
2025-07-12 17:13:57 +0700

    filter restaurants

--- web-programming/restaurant-app/home.js
+++ web-programming/restaurant-app/home.js
@@ -9,7 +9,10 @@ const renderRestaurant = (res) => `
     </a>
 `;
 
-restaurant_list.forEach(res => {
+let filtered_list = structuredClone(restaurant_list);
+
+// Render the full list of restaurants on start up
+filtered_list.forEach(res => {
     const restaurantTemplate = renderRestaurant(res);
     restaurantDiv.innerHTML += restaurantTemplate;
 });
@@ -28,5 +31,46 @@ modal.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none';
     }         
+});
+
+
+// Search function
+const searchForm = document.querySelector('#search-form');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevent the page from reloading (default behavior)
+    // console.log(e);
+
+    const query = e.target[0].value; // get the value from the input box
+    console.log(query);
+
+    if (!query) {
+        filtered_list = structuredClone(restaurant_list);
+    } else {
+        filtered_list = restaurant_list.filter(res => {
+            // matching lowercase
+            if (query.toLowerCase() === res.title.toLowerCase()) {
+                return res;
+            }
+            // find by substring
+            else if (res.title.toLowerCase().includes(query.toLowerCase())) {
+                return res;
+            }
+        });
+    }
+
+    console.log('filtered:', filtered_list);
+
+    // clear the current list
+    restaurantDiv.innerHTML = '';
+
+    // render the new list
+    filtered_list.forEach(res => {
+        const restaurantTemplate = renderRestaurant(res);
+        restaurantDiv.innerHTML += restaurantTemplate;
+    });
+
+    // reset the search box
+    e.target[0].value = '';
 });
 
