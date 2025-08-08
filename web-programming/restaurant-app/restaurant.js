d4ddac6507b31c36e5f98928b65182af6515825f
hoangdesu <hoangdesu@gmail.com>
2025-08-08 18:29:06 +0700

    restaurant detail page, embed google map, hover image effect

--- web-programming/restaurant-app/restaurant.js
+++ web-programming/restaurant-app/restaurant.js
@@ -13,7 +13,7 @@ console.log('found:', restaurant);
 
 const title = document.querySelector('#title');
 const address = document.querySelector('#address');
-const image = document.querySelector('#image');
+const mainImage = document.querySelector('#main-image');
 const favBtn = document.querySelector('#fav-btn');
 
 
@@ -21,9 +21,48 @@ title.textContent = restaurant.title;
 document.title = restaurant.title;
 address.textContent = restaurant.address;
 
-image.src = restaurant.image;
+mainImage.src = restaurant.image;
 
 favBtn.addEventListener('click', () => {
     favorites.push(restaurant);
     console.log(favorites);
 });
+
+
+// Thumbnails
+const thumbnailsContainer = document.querySelector('#thumbnails-container');
+
+// console.log(thumbnailsContainer.);
+
+
+const thumbnailList = [
+    'test-1.jpg',
+    'test-2.jpg',
+    'test-3.jpg',
+    'test-4.jpg',
+];
+
+thumbnailList.forEach(thumbnail => {
+    const img = `<img src="./images/${thumbnail}" alt="">`;
+
+    thumbnailsContainer.innerHTML += img;
+});
+
+const thumbnailImages = thumbnailsContainer.querySelectorAll('img');
+console.log(thumbnailImages);
+
+thumbnailImages.forEach(thumbnailImg => {
+    thumbnailImg.addEventListener('mouseenter', (evt) => {
+        console.log('hovering on...', thumbnailImg.src);
+        mainImage.src = thumbnailImg.src;
+    });
+});
+
+thumbnailImages.forEach(thumbnailImg => {
+    thumbnailImg.addEventListener('mouseleave', (evt) => {
+        mainImage.src = restaurant.image;
+    });
+})
+
+
+
