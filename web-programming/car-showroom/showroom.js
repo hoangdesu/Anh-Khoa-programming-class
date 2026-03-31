f988c4cbd2ee8d23e9905b518a7c07b172467ecd
hoangdesu <hoangdesu@gmail.com>
2026-03-31 18:02:46 +0700

    car-showroom: basic filled animation

--- web-programming/car-showroom/showroom.js
+++ web-programming/car-showroom/showroom.js
@@ -7,7 +7,7 @@ const storySlides = [
     {
         image: 'https://cars-assets-production.mclaren.com/2208/750s-development-stories-durability-banner.jpg',
         title: '750S Development Stories - Durability',
-        content: 'All over the world, from labs to real-life, teams work tirelessly to make sure every McLaren lasts a lifetime. The 750S is no exception.'
+        content: 'All over the world, from labs to real-life, teams work tirelessly to make sure every McLaren lasts a lifetime. The 750S is no exception. Lorem ipsum dolor sit amet consectetur adipisicing elit. '
     },
     {
         image:"https://cars-assets-production.mclaren.com/2202/750s-every-limit-can-be-surpassed-banner.jpg",
@@ -34,9 +34,33 @@ storySlides.forEach(story => {
             <img src="${story.image}" alt="${story.title}">
             <h3>${story.title}</h3>
             <p>${story.content}</p>
-            <button>Read more</button>
+            <a href="#">Read more</a>
         </div>
     `;
 
     swiperWrapper.innerHTML += swiperSlide;
-})
+});
+
+
+const speedH1 = document.querySelector('#speed-h1');
+const filledDiv = document.querySelector('#filled');
+
+let speedCounter = 0;
+const speedCapped = 332;
+let filledPercentage = speedCounter / speedCapped;
+
+const intervalId = setInterval(() => {
+    speedCounter++;
+    speedH1.textContent = speedCounter;
+
+    filledPercentage = speedCounter / speedCapped * 100;
+    filledDiv.style.height = `${filledPercentage}%`;
+
+    if (speedCounter === speedCapped) {
+        clearInterval(intervalId);
+    }
+}, 5); // 1000ms = 1s
+
+// 332 = 100%
+// 3.32 = 1%
+
