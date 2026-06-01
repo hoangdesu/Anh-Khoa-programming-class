bdaaa79ca3273ade4b72b6b56bdf9e441062d66f
hoangdesu <hoangdesu@gmail.com>
2026-06-01 22:02:28 +0800

    react state

--- hi-react/src/App.jsx
+++ hi-react/src/App.jsx
@@ -5,23 +5,53 @@ import heroImg from './assets/hero.png'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(911);
+  // const [count, setCount] = useState(911);
 
-  // variable
-  let favCar = 'Porsche 911 GT3 RS';
+  // // variable
+  // let favCar = 'Porsche 911 GT3 RS';
 
-  const onAdd = () => {
-    setCount(count + 1);
-  };
+  // const onAdd = () => {
+  //   setCount(count + 1);
+  // };
+
+  // let myCounter = 1;
+
+  // return (
+  //   <div>
+  //     <p>My favorite car is <b>{favCar}</b></p>
+
+  //     <h1>Count = {count}</h1>
+  //     <button onClick={onAdd}>Add 1</button>
+
+  //     <button onClick={() => {
+  //       myCounter++;
+  //       console.log('my counter: ', myCounter);
+        
+  //     }}>My counter: {myCounter}</button>
+
+  //   </div>
+  // )
+
+
+  // React hooks: use...
+
+  const [counter, setCounter] = useState(90); // default value of counter = 90
+
+  const increaseCounter = () => {
+    // counter = 10; not gonna work
+
+    setCounter(counter + 1);
+  }
 
   return (
     <div>
-      <p>My favorite car is <b>{favCar}</b></p>
+      <p>Counter: {counter}</p>
 
-      <h1>Count = {count}</h1>
-      <button onClick={onAdd}>Add 1</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrease counter</button>
+      <button onClick={increaseCounter}>Increase counter</button>
     </div>
   )
+
 }
 
 export default App
--- may27-hi-react/src/CarCard.jsx
+++ may27-hi-react/src/CarCard.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 export default function CarCard({ car, inStock }) {
+  const [amount, setAmount] = useState(1);
+
+  const addOne = () => {
+    setAmount(amount + 1);
+  }
+
+  const decrease = () => {
+    if (amount > 0) {
+      setAmount(amount - 1);
+    }
+  }
+
   return (
     <div className="car-card">
       <img
@@ -7,7 +21,14 @@ export default function CarCard({ car, inStock }) {
       />
       <h1>{car.name}</h1>
       <p>${car.price}</p>
-      <button disabled={!inStock} onClick={() => alert(`Thanks for buying ${car.name}!`)}>
+
+      <div className="amount-container">
+        <button onClick={decrease}>-</button>
+        <span>{amount}</span>
+        <button onClick={addOne}>+</button>
+      </div>
+
+      <button disabled={!inStock} onClick={() => alert(`Thanks for buying ${amount} ${car.name}!`)}>
         {inStock ? 'Buy Now!!' : 'OUT OF STOCK'}
       </button>
     </div>
